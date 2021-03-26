a85a8401e805ae5d8cf279bc007bd0493352490e
Jeremiah ONeal <ardurover-robot@jeremiahonealtechsupport.com>
2021-03-25 18:06:52 -0700

    Add files to repo

--- python/forward.py
+++ python/forward.py
@@ -6,6 +6,12 @@ MotorPin1 = 17
 MotorPin2 = 18
 MotorEnable = 27
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(MotorPin1, GPIO.OUT)
+GPIO.setup(MotorPin2, GPIO.OUT)
+GPIO.setup(MotorEnable, GPIO.OUT)
+GPIO.output(MotorEnable,GPIO.LOW)
+
 GPIO.output(MotorEnable, GPIO.HIGH)
 GPIO.output(MotorPin1, GPIO.HIGH)
 GPIO.output(MotorPin2, GPIO.HIGH)
@@ -13,3 +19,4 @@ time.sleep(5)
 GPIO.output(MotorEnable, GPIO.LOW)
 GPIO.output(MotorPin1, GPIO.LOW)
 GPIO.output(MotorPin2, GPIO.LOW)
+GPIO.cleanup()
