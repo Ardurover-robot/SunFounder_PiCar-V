31e1a1c32b5730b299c8f58792b355ff8f84044b
Jeremiah ONeal <ardurover-robot@jeremiahonealtechsupport.com>
2021-03-30 13:17:03 -0700

    Add files to repo

--- gamepad/gamepad.py
+++ gamepad/gamepad.py
@@ -1,14 +1,27 @@
+#Modified code from here: https://github.com/sunfounder/SunFounder_PiCar-V/blob/master/ball_track/ball_tracker.py
 #Created by Kepler Electronics, https://www.youtube.com/keplerelectronics
-import explorerhat
+#import explorerhat
+#import os
+#import time
+from picar import front_wheels, back_wheels
+from picar.SunFounder_PCA9685 import Servo
+import picar
+from time import sleep
+#import cv2
+import numpy as np
+import picar #a second time?
 import os
-import time
 
-explorerhat.motor.one.invert()
+#explorerhat.motor.one.invert()
 
 from evdev import InputDevice, categorize, ecodes
 
 gamepad = InputDevice('/dev/input/event0')
 print(gamepad)
+
+picar.setup()
+turnServo = Servo.Servo(0);
+
 
 
 
@@ -16,17 +29,20 @@ for event in gamepad.read_loop():
     if event.type == ecodes.EV_ABS:
         if event.code == 0:
             if event.value == 0:
-                
+                turnServo.write(60); 
                 print("Left");#explorerhat.motor.one.forwards(event.value-155)
             elif event.value == 2:
+                turnServo.write(110); 
                 print("Right");#explorerhat.motor.one.backwards((100-event.value))
             else:
-                explorerhat.motor.one.stop()
+                turnServo.write(80);
+                #explorerhat.motor.one.stop()
         elif event.code == 1:
             if event.value == 0:
                 print("Up");#explorerhat.motor.two.forwards(event.value-155)
             elif event.value == 2:
                 print("Down");#explorerhat.motor.two.backwards((100-event.value))
             else:
-                explorerhat.motor.two.stop()
+                turnServo.write(0);
+                #explorerhat.motor.two.stop()
 
