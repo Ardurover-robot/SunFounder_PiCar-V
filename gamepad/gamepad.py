262815bea8731d707f16654a4daba494d69a05a8
Jeremiah ONeal <ardurover-robot@jeremiahonealtechsupport.com>
2021-03-30 16:40:47 -0700

    Add files to repo

--- gamepad/gamepad.py
+++ gamepad/gamepad.py
@@ -1,34 +1,26 @@
 #Modified code from here: https://github.com/sunfounder/SunFounder_PiCar-V/blob/master/ball_track/ball_tracker.py
 #Created by Kepler Electronics, https://www.youtube.com/keplerelectronics
-#import explorerhat
-#import os
-#import time
 from picar import front_wheels, back_wheels
 from picar.SunFounder_PCA9685 import Servo
 import picar
+import time
 from time import sleep
-#import cv2
 import numpy as np
 import picar #a second time?
 import os
-
-#explorerhat.motor.one.invert()
-
 from evdev import InputDevice, categorize, ecodes
-
 gamepad = InputDevice('/dev/input/event0')
 print(gamepad)
-
 turnServo = Servo.Servo(0);
 picar.setup()
 bw = picar.back_wheels.Back_Wheels()
-speedup = 0;
-acc = 0
-
-
-
-
+speedup = 0
+bw.forward()
+bw.speed = 50 
+time.sleep(5)
+bw.stop()
 for event in gamepad.read_loop():
+    print(event.code)
     if event.type == ecodes.EV_ABS:
         if event.code == 0:
             if event.value == 0:
@@ -42,24 +34,27 @@ for event in gamepad.read_loop():
                 #explorerhat.motor.one.stop()
         elif event.code == 1:
             if event.value == 0:
-                acc = 7 
-                speedup = 35 
+                bw.backward() 
+                speedup = 100 
                 print("Up");#explorerhat.motor.two.forwards(event.value-155)
             elif event.value == 2:
-                acc = 4 
-                speedup = 35
+                bw.forward() 
+                speedup = 100 
                 print("Down");#explorerhat.motor.two.backwards((100-event.value))
             else:
                 bw.stop()#turnServo.write(0);
+                speedup = 0 
                 #explorerhat.motor.two.stop()
                 print("Stop")
-    if(speedup < 100):
-        speedup = speedup + acc
-    speedup = speedup - 1
-    if(acc == 7):
-        bw.backward()
-    if(acc == 4):
-        bw.forward()
-    if(speedup < 100):
-        bw.speed = speedup
+    elif event.code == 310:
+        speedup = speedup + 10
+        print("Upp")
+    elif event.code == 311:
+        speedup = speedup - 10
+        print("Doown")
+
+    if(speedup <= 100):
+        if(speedup >= 0):
+            bw.speed = speedup
+
 
--- gamepad/gamepad.sh
+++ gamepad/gamepad.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+while True
+do
 cd /var/www/html/gamepad
 python gamepad.py
 sleep 1
 echo "******* LOG ********"
 scrot /var/www/html/screen.jpg
-sleep 30
+sleep 10
+done
